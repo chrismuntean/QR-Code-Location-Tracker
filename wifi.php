591e96956631206837d0ec3bb5827f80f172dddf
Chris Muntean <57206061+chrismuntean@users.noreply.github.com>
2021-12-13 00:47:18 -0600

    Add files via upload

---
+++ wifi.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Oops! Please connect to wifi!</title>
+		<link rel="icon" href="favicon.png" type="image/x-icon"/>
+      <link href="/css/all.css" rel="stylesheet" type="text/css"/>
+			<link href="/css/wifi.css" rel="stylesheet" type="text/css"/>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+  </head>
+<body>
+<center>
+	<div class="message">Oops! Please connect to wifi (or disconnect your VPN) and try again!</div>
+	
+	<div style="margin-top: 3rem;">
+		<div class="track-link-message">Or if your just trying to see my scanned locations click </div><div class="track-link-cont"><a class="track-link" href="index.php">here</a></div>
+	</div>
+</center>
+</body>
+</html>
